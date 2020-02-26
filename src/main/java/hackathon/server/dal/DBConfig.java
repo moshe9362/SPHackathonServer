9b469a7cd5895ac016fa0ee98733b598a7e1fcda
shaked <shvshv44@gmail.com>
2020-02-26 11:44:27 +0200

    feat:  db-config  test controller

--- src/main/java/hackathon/server/dal/DBConfig.java
+++ src/main/java/hackathon/server/dal/DBConfig.java
@@ -1,7 +1,9 @@
 package hackathon.server.dal;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 public class DBConfig {
 }
