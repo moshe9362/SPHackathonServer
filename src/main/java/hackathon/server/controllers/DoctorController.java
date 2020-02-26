faa5da13ea63f9eba697a863172042f3ee17ced8
efishuv <efishuv@gmail.com>
2020-02-26 21:32:47 +0100

    Add patients get route in DoctorController

--- src/main/java/hackathon/server/controllers/DoctorController.java
+++ src/main/java/hackathon/server/controllers/DoctorController.java
@@ -1,6 +1,9 @@
 package hackathon.server.controllers;
 
+import hackathon.server.dal.crud.PatientRepository;
 import hackathon.server.models.MockEntity;
+import hackathon.server.models.db.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +14,16 @@ import java.util.List;
 @Controller
 public class DoctorController {
 
-    @GetMapping("/patient")
-    public List<Integer> getAllPatients() {
-        //TODO add logic
-        return new ArrayList<>();
+    @Autowired
+    PatientRepository patientRepository;
+
+    @GetMapping("/patients")
+    public List<Patient> getAllPatients() {
+        List<Patient> result = patientRepository.findAll();
+        for (Patient patient : result) {
+            patient.setPassword(null);
+        }
+        return result;
     }
 
 
