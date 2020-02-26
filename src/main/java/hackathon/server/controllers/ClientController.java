50b7afd3e05cb213160b65783fa1d1ba6214d771
ori <ori_eshel@walla.com>
2020-02-26 17:07:49 +0200

    get all protocols

--- src/main/java/hackathon/server/controllers/ClientController.java
+++ src/main/java/hackathon/server/controllers/ClientController.java
@@ -3,39 +3,39 @@ package hackathon.server.controllers;
 import hackathon.server.dal.crud.PatientRepository;
 import hackathon.server.models.MockEntity;
 import hackathon.server.models.db.Patient;
+import hackathon.server.models.db.Protocol;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class ClientController {
 
     private PatientRepository patientRepository;
+    private Protocol protocol;
 
     @PostMapping("/client/stab")
     public MockEntity stab() {
         return new MockEntity();
     }
 
-
-    @GetMapping("/shaq/create/patient")
-    @ResponseBody
-    public String createPatient() {
-        Patient patient = new Patient();
-        patient.setIdNumber(209081900l);
-        patient.setLastName("Vitkon");
-        patient.setFirstName("Shaked");
-        patientRepository.save(patient);
-        return "Created Successffuly";
-    }
-
     public List getAllProtocols(){
-        patientRepository.getAllProtocolName();
-        patientRepository.getAllProtocolId();
-        return null;
+        List<String> names = patientRepository.getAllProtocolName();
+        List<Long> ids = patientRepository.getAllProtocolId();
+
+        Map<String, Long> protocolList = new HashMap<>();
+
+        for (int i = 0; i < ids.size(); i++) {
+            protocolList.put(names.get(i), ids.get(i));
+        }
+
+        return (List) protocolList;
     }
 
 }
