482c4705d68b8b4e5f7596e31689691367cb2470
moshe margaliot <moshe9362@gmail.com>
2020-02-26 15:55:05 +0200

    add mapping skeletons

--- src/main/java/hackathon/server/controllers/ClientController.java
+++ src/main/java/hackathon/server/controllers/ClientController.java
@@ -1,14 +1,38 @@
 package hackathon.server.controllers;
 
 import hackathon.server.models.MockEntity;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class ClientController {
 
-    @PostMapping("/client/stab")
-    public MockEntity stab() {
+    @PostMapping("/user/signUp")
+    public MockEntity signUp(@RequestParam("id") String id,
+                             @RequestParam("firstName") String FirstName,
+                             @RequestParam("lastName") String lastName,
+                             @RequestParam("birthDate") String birthDate,
+                             @RequestParam("gender") int gender,
+                             @RequestParam("password") String password) {
+        String encodedPassword = DigestUtils.sha256Hex(password);
+
+
+
+        return new MockEntity();
+    }
+
+    @PostMapping("/user/login")
+    public MockEntity login(@RequestParam("id") String id,
+                            @RequestParam("password") String password) {
+        String encodedPassword = DigestUtils.sha256Hex(password);
+
+
         return new MockEntity();
     }
 
--- src/main/java/hackathon/server/controllers/DoctorController.java
+++ src/main/java/hackathon/server/controllers/DoctorController.java
@@ -2,14 +2,20 @@ package hackathon.server.controllers;
 
 import hackathon.server.models.MockEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DoctorController {
 
-    @PostMapping("/doctor/stab")
-    public MockEntity stab() {
-        return new MockEntity();
+    @GetMapping("/patient")
+    public List<Integer> getAllPatients() {
+        return new ArrayList<>();
     }
+
+
 
 }
