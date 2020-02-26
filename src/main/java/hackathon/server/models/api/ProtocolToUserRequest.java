603447bb37b03147b889230ab014a09afc3f3d2b
efishuv <efishuv@gmail.com>
2020-02-26 17:51:57 +0100

    Add validation annotations for all request classes

--- src/main/java/hackathon/server/models/api/ProtocolToUserRequest.java
+++ src/main/java/hackathon/server/models/api/ProtocolToUserRequest.java
@@ -1,10 +1,18 @@
 package hackathon.server.models.api;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class ProtocolToUserRequest {
+    @Pattern(regexp = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")
     private String userUuid;
+    @NotBlank(message = "protocolId is mandatory")
     private Long protocolId;
+    @NotBlank(message = "startDate is mandatory")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private String startDate;
 }
