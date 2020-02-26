11ccaf0affbada03207fe6c82540287ecb8a4325
shaked <shvshv44@gmail.com>
2020-02-26 11:11:50 +0200

    feat:  local db connector  patient entity

--- src/main/java/hackathon/server/dal/A.java
+++
@@ -1,4 +0,0 @@
-package hackathon.server.dal;
-
-public class A {
-}
---
+++ src/main/java/hackathon/server/models/db/Patient.java
@@ -0,0 +1,23 @@
+package hackathon.server.models.db;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String uuid;
+    private long idNumber;
+    private String firstName;
+    private String lastName;
+
+}
