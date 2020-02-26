091df72c6baa7ff29d971b08681f05bdae95deab
moshe margaliot <moshe9362@gmail.com>
2020-02-26 20:40:48 +0200

    remove function

--- src/main/java/hackathon/server/dal/crud/PatientToProtocolRepository.java
+++ src/main/java/hackathon/server/dal/crud/PatientToProtocolRepository.java
@@ -14,8 +14,5 @@ public interface PatientToProtocolRepository extends CrudRepository<PatientToPro
 
     List<PatientToProtocol> findAll();
 
-//    @Query("select ptp.protocol_id from patient_to_protocol ptp where ptp.id.end_date is null and ptp.id.patient_uuid = :patient_uuid")
-//    List<Long> getProtocolIds(@Param("patient_uuid") String patientUuid);
-
     List<PatientToProtocol> findByPatientUuidAndEndDate(String patiendUuid, Date endDate);
 }
