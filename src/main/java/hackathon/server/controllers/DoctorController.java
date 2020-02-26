05c19cf0975e5dc4ef0af3706d83a889bac75956
efishuv <efishuv@gmail.com>
2020-02-26 22:40:40 +0100

    Add patients get route in DoctorController

--- src/main/java/hackathon/server/controllers/DoctorController.java
+++ src/main/java/hackathon/server/controllers/DoctorController.java
@@ -1,15 +1,15 @@
 package hackathon.server.controllers;
 
 import hackathon.server.dal.crud.PatientRepository;
-import hackathon.server.models.MockEntity;
+import hackathon.server.models.api.PatientReply;
+import hackathon.server.models.api.enums.Gender;
 import hackathon.server.models.db.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class DoctorController {
@@ -18,12 +18,27 @@ public class DoctorController {
     PatientRepository patientRepository;
 
     @GetMapping("/patients")
-    public List<Patient> getAllPatients() {
-        List<Patient> result = patientRepository.findAll();
-        for (Patient patient : result) {
-            patient.setPassword(null);
+    @ResponseBody
+    public ArrayList<PatientReply> getAllPatients() {
+        List<Patient> dbResult = patientRepository.findAll();
+        ArrayList<PatientReply> result = new ArrayList();
+        for (Patient patient : dbResult) {
+             result.add(createPatientReply(patient));
         }
         return result;
+    }
+
+    private PatientReply createPatientReply(Patient patient) {
+        PatientReply patientReply = new PatientReply();
+        patientReply.setUuid(patient.getUuid());
+        patientReply.setIdNumber(patient.getIdNumber());
+        patientReply.setFirstName(patient.getFirstName());
+        patientReply.setLastName(patient.getLastName());
+        patientReply.setBirthDate(patient.getBirthDate());
+        patientReply.setGender(Gender.values()[patient.getGender()]);
+        patientReply.setEmail(patient.getEmail());
+        patientReply.setPhone(patient.getPhone());
+        return patientReply;
     }
 
 
