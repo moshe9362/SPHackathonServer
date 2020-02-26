a1c8f9180d83034521b50bbe2582d32f540cb77f
ori <ori_eshel@walla.com>
2020-02-26 21:29:40 +0200

    getMapping

--- src/main/java/hackathon/server/controllers/ClientController.java
+++ src/main/java/hackathon/server/controllers/ClientController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -89,6 +90,7 @@ public class ClientController {
         return patientLoginReply;
     }
 
+    @GetMapping()
     public List getAllProtocols(){
         List<Protocol> list = protocolRepository.findAll();
         return list;
